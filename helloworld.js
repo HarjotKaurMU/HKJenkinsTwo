de5daa3ce56cd822aff0b533bf75130f33d7a3bf
HarjotKaurMU <68206702+HarjotKaurMU@users.noreply.github.com>
2020-09-08 00:30:01 -0700

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello and welcome to Harjot's page..stay tuned for more information. have a great Day !!"
-   response.end('Hello and welcome to Harjot''s page..stay tuned for more information. have a great Day !!\n')
+   response.end('Hello and welcome to HK page..stay tuned for more information. have a great Day !!\n')
 }).listen(3000)
 
 // Console will print the message
